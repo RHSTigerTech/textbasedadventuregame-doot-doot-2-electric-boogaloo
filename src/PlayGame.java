e926f20c1ee94f3aa5588f5d234b0ac0cec5a3c9
Postboi <treydriggs@yahoo.com>
2018-11-29 08:04:07 -0800

    wincon improved

--- src/PlayGame.java
+++ src/PlayGame.java
@@ -7,6 +7,14 @@ public class PlayGame {
 
         while (!winCon) {
 
+            System.out.println("You are in a spaceship. " +
+                    "Your only chance at escaping is to dock onto a nearby ship that responded to your SOS." +
+                    " You are out of fuel and space Imps have pulled all sorts of trickery onto the ship. |OBJECTIVE: Escape the ship. Use /help for a list of commands");
+
+
+
+            winCon = true;
         }
+        System.out.println("You win!");
     }
 }
