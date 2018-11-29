35689fb7948f35c2ba9671898d9a7757f08a443c
Postboi <treydriggs@yahoo.com>
2018-11-29 09:02:51 -0800

    player toString

--- src/Player.java
+++ src/Player.java
@@ -12,4 +12,16 @@ public class Player extends LivingThing {
     public void setInventory(Items[] inventory) {
         this.inventory = inventory;
     }
+
+    @Override
+    public String toString() {
+        return "Health: " + getHealth() +
+                "\nStrength: " + getStrength() +
+                "\nDefense: " + getDefense() +
+                "\nINVENTORY 1" + inventory[0] +
+                "\nINVENTORY 2" + inventory[1] +
+                "\nINVENTORY 3" + inventory[2] +
+                "\nINVENTORY 4" + inventory[3] +
+                "\nINVENTORY 5" + inventory[4];
+    }
 }
