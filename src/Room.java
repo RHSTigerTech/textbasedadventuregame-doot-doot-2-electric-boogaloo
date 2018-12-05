b863adb59bcd30c0f34e5c8050deb343771e6da9
Safyire <safyire.n8@gmail.com>
2018-12-05 07:41:09 -0800

    Formatting

--- src/Room.java
+++ src/Room.java
@@ -5,7 +5,7 @@ public class Room extends NamedThing {
      * @param name The name of the room.
      * @param description The description of the room.
      */
-    public Room(String name, String description){
+    public Room(String name, String description) {
         super(name, description);
     }
 }
